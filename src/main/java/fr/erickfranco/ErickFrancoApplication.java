1c465b9a428f200040b1f2bbb5e31455f92a72be
gache <erickfrancodelgado@hotmail.com>
2021-09-12 22:15:25 +0200

    first commit

---
+++ src/main/java/fr/erickfranco/ErickFrancoApplication.java
@@ -0,0 +1,13 @@
+package fr.erickfranco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErickFrancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErickFrancoApplication.class, args);
+	}
+
+}
